16e9b1c909ca942c3e431642716fa037adc4343e
halvors <halvors@halvors.org>
2017-06-04 21:24:29 +0200

    Renamed Item to QuantumItem.

--- src/main/java/org/halvors/quantum/common/item/ItemQuantum.java
+++ src/main/java/org/halvors/quantum/common/item/ItemQuantum.java
@@ -1,16 +1,17 @@
 package org.halvors.quantum.common.item;
 
+import net.minecraft.item.Item;
 import org.halvors.quantum.Quantum;
 
 /**
- * This is a basic Item that is meant to be extended by other Items.
+ * This is a basic ItemQuantum that is meant to be extended by other Items.
  *
  * @author halvors
  */
-public class Item extends net.minecraft.item.Item {
+public class ItemQuantum extends Item {
 	protected final String name;
 
-	protected Item(String name) {
+	protected ItemQuantum(String name) {
 		this.name = name;
 
 		setUnlocalizedName(name);
