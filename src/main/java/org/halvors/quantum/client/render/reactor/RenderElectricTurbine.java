9e1c2744612f0b2b2d0e131d1a650463fe38748c
halvors <halvors@halvors.org>
2017-06-10 02:56:07 +0200

    Fixed rendering of small turbine, and optimized it's wavefront model.

--- src/main/java/org/halvors/quantum/client/render/reactor/RenderElectricTurbine.java
+++ src/main/java/org/halvors/quantum/client/render/reactor/RenderElectricTurbine.java
@@ -2,15 +2,16 @@ package org.halvors.quantum.client.render.reactor;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 import org.apache.commons.lang3.ArrayUtils;
+import org.halvors.quantum.Quantum;
 import org.halvors.quantum.common.Reference;
 import org.halvors.quantum.common.tile.reactor.TileTurbine;
+import org.halvors.quantum.lib.utility.RenderUtility;
 import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
@@ -29,7 +30,7 @@ public class RenderElectricTurbine extends TileEntitySpecialRenderer {
             GL11.glTranslated(x + 0.5D, y, z + 0.5D);
 
             if (tileTurbine.getMultiBlock().isConstructed()) {
-                Minecraft.getMinecraft().renderEngine.bindTexture(textureLarge);
+                RenderUtility.bind(textureLarge);
 
                 String[] blades = { "Blade1", "Blade2", "Blade3", "Blade4", "Blade5", "Blade6" };
                 String[] mediumBlades = { "MediumBlade1", "MediumBlade2", "MediumBlade3", "MediumBlade4", "MediumBlade5", "MediumBlade6" };
@@ -49,48 +50,28 @@ public class RenderElectricTurbine extends TileEntitySpecialRenderer {
                 modelLarge.renderAllExcept(ArrayUtils.addAll(ArrayUtils.addAll(blades, mediumBlades), largeBlades));
             } else {
                 GL11.glScalef(1.0F, 1.1F, 1.0F);
-                Minecraft.getMinecraft().renderEngine.bindTexture(textureSmall);
+                RenderUtility.bind(textureSmall);
 
-                String[] bladesA = new String[3];
-
-                for (int i = 0; i < bladesA.length; i++) {
-                    bladesA[i] = ("BLADE A" + (i + 1) + " SPINS");
-                }
-
-                String[] sheildsA = new String[6];
-
-                for (int i = 0; i < sheildsA.length; i++) {
-                    sheildsA[i] = ("SHIELD A" + (i + 1) + " SPINS");
-                }
-
-                String[] bladesB = new String[3];
-
-                for (int i = 0; i < bladesB.length; i++) {
-                    bladesB[i] = ("BLADE B" + (i + 1) + " SPINS");
-                }
-
-                String[] sheildsB = new String[6];
-
-                for (int i = 0; i < sheildsB.length; i++) {
-                    sheildsB[i] = ("SHIELD B" + (i + 1) + " SPINS");
-                }
-
-                String[] renderA = ArrayUtils.addAll(bladesA, sheildsA);
-                String[] renderB = ArrayUtils.addAll(bladesB, sheildsB);
+                String[] blades = { "Blade1", "Blade2", "Blade3" };
+                String[] mediumBlades = { "MediumBlade1", "MediumBlade2", "MediumBlade3" };
+                String[] shields = { "Shield1", "Shield2", "Shield3", "Shield4", "Shield5", "Shield6" };
+                String[] mediumShields = { "MediumShield1", "MediumShield2", "MediumShield3", "MediumShield4", "MediumShield5", "MediumShield6" };
 
                 GL11.glPushMatrix();
-                GL11.glRotated(Math.toDegrees(tileTurbine.rotation), 0.0D, 1.0D, 0.0D);
-                modelSmall.renderOnly(renderA);
+                GL11.glRotated(Math.toDegrees(tileTurbine.rotation), 0, 1, 0);
+                modelSmall.renderOnly(blades);
+                modelSmall.renderOnly(shields);
                 GL11.glPopMatrix();
 
                 GL11.glPushMatrix();
-                GL11.glRotated(-Math.toDegrees(tileTurbine.rotation), 0.0D, 1.0D, 0.0D);
-                modelSmall.renderOnly(renderB);
+                GL11.glRotated(-Math.toDegrees(tileTurbine.rotation), 0, 1, 0);
+                modelSmall.renderOnly(mediumBlades);
+                modelSmall.renderOnly(mediumShields);
                 GL11.glPopMatrix();
 
-                modelSmall.renderAllExcept(ArrayUtils.addAll(renderA, renderB));
+                modelSmall.renderAllExcept(ArrayUtils.addAll(ArrayUtils.addAll(blades, shields), ArrayUtils.addAll(mediumShields, mediumBlades)));
             }
-
+            
             GL11.glPopMatrix();
         }
     }
