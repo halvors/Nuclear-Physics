8bc616f9abf1bbe935142fd53f42d528afe7526f
halvors <halvors@halvors.org>
2017-06-18 21:04:04 +0200

    Only accept uranium ore in chemical extractor.

--- src/main/java/org/halvors/quantum/common/container/machine/ContainerChemicalExtractor.java
+++ src/main/java/org/halvors/quantum/common/container/machine/ContainerChemicalExtractor.java
@@ -3,9 +3,12 @@ package org.halvors.quantum.common.container.machine;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.inventory.SlotFurnace;
+import net.minecraft.item.ItemStack;
+import org.halvors.quantum.Quantum;
 import org.halvors.quantum.common.tile.machine.TileChemicalExtractor;
-import org.halvors.quantum.lib.gui.ContainerBase;
-import org.halvors.quantum.lib.gui.slot.SlotEnergyItem;
+import org.halvors.quantum.lib.container.ContainerBase;
+import org.halvors.quantum.lib.container.slot.SlotEnergyItem;
+import org.halvors.quantum.lib.container.slot.SlotSpecific;
 
 public class ContainerChemicalExtractor extends ContainerBase {
     public ContainerChemicalExtractor(InventoryPlayer inventoryPlayer, TileChemicalExtractor tile) {
@@ -14,8 +17,8 @@ public class ContainerChemicalExtractor extends ContainerBase {
         // Battery
         addSlotToContainer(new SlotEnergyItem(tile, 0, 80, 50));
 
-        // Process Input (Cell or Uranium)
-        addSlotToContainer(new Slot(tile, 1, 53, 25));
+        // Process Input (Uranium)
+        addSlotToContainer(new SlotSpecific(tile, 1, 53, 25, new ItemStack(Quantum.blockUraniumOre)));
 
         // Process Output
         addSlotToContainer(new SlotFurnace(inventoryPlayer.player, tile, 2, 107, 25));
