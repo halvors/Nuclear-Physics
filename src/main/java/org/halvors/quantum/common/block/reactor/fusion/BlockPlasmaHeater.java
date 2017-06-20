09040b65074960c3c12b314cf07eeb8abee94e85
halvors <halvors@halvors.org>
2017-06-20 12:35:05 +0200

    Fixed plasma heater break texture.

--- src/main/java/org/halvors/quantum/common/block/reactor/fusion/BlockPlasmaHeater.java
+++ src/main/java/org/halvors/quantum/common/block/reactor/fusion/BlockPlasmaHeater.java
@@ -2,6 +2,7 @@ package org.halvors.quantum.common.block.reactor.fusion;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
@@ -13,18 +14,24 @@ import org.halvors.quantum.lib.prefab.block.BlockTile;
 import org.halvors.quantum.client.render.BlockRenderingHandler;
 import org.halvors.quantum.lib.utility.FluidUtility;
 
-public class BlockPlasmaHeater extends BlockTile {
+public class BlockPlasmaHeater extends BlockContainer {
     public BlockPlasmaHeater() {
         super(Material.iron);
 
         setUnlocalizedName("plasmaHeater");
-        setTextureName(Reference.PREFIX + "plasmaHeater");
+        setTextureName(Reference.PREFIX + "machine");
         setCreativeTab(Quantum.getCreativeTab());
     }
 
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
         return FluidUtility.playerActivatedFluidItem(world, x, y, z, player, side);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public int getRenderType() {
+        return BlockRenderingHandler.getId();
     }
 
     @Override
@@ -37,12 +44,6 @@ public class BlockPlasmaHeater extends BlockTile {
     @SideOnly(Side.CLIENT)
     public boolean renderAsNormalBlock() {
         return false;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public int getRenderType() {
-        return BlockRenderingHandler.getId();
     }
 
     @Override
