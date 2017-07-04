e63b66a5c8e68e18626fb565841c45ffe89a12a1
Halvor Lyche Strandvoll <halvors@halvors.org>
2017-07-04 06:46:51 +0200

    Fix stepsound for uranium ore.

--- src/main/java/org/halvors/quantum/common/block/BlockUraniumOre.java
+++ src/main/java/org/halvors/quantum/common/block/BlockUraniumOre.java
@@ -1,5 +1,6 @@
 package org.halvors.quantum.common.block;
 
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import org.halvors.quantum.common.ConfigurationManager;
 
@@ -9,7 +10,7 @@ public class BlockUraniumOre extends BlockRadioactive {
     public BlockUraniumOre() {
         super("uranium_ore", Material.ROCK);
 
-        //setStepSound(soundStoneFootstep);
+        setSoundType(SoundType.STONE);
         setHardness(2.0F);
         setHarvestLevel("pickaxe", 2);
 
