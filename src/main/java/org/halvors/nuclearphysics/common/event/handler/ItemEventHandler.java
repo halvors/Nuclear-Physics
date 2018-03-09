d205edc7968325e688c881bba8c34ca2bc74c685
Halvor Lyche Strandvoll <halvors@halvors.org>
2018-03-09 16:20:27 +0100

    Added configuration option for preventing anitmatter from exploding, and thus being used for power generation.

--- src/main/java/org/halvors/nuclearphysics/common/event/handler/ItemEventHandler.java
+++ src/main/java/org/halvors/nuclearphysics/common/event/handler/ItemEventHandler.java
@@ -4,26 +4,24 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.halvors.nuclearphysics.common.ConfigurationManager.General;
 import org.halvors.nuclearphysics.common.effect.explosion.AntimatterExplosion;
 import org.halvors.nuclearphysics.common.init.ModItems;
 
 public class ItemEventHandler {
     @SubscribeEvent
     public void onItemExpireEvent(ItemExpireEvent event) {
-        //if (!FlagRegistry.getModFlag(FlagRegistry.DEFAULT_NAME).containsValue(event.entityItem.worldObj, Atomic.BAN_ANTIMATTER_POWER, "true", new Vector3(event.entityItem))) {
+        if (General.enableAntimatterPower) {
+            EntityItem entityItem = event.getEntityItem();
 
-        EntityItem entityItem = event.getEntityItem();
+            if (entityItem != null) {
+                ItemStack itemStack = entityItem.getEntityItem();
 
-        if (entityItem != null) {
-            ItemStack itemStack = entityItem.getEntityItem();
-
-            if (itemStack.getItem() == ModItems.itemAntimatterCell) {
-                AntimatterExplosion explosion = new AntimatterExplosion(entityItem.getEntityWorld(), entityItem, entityItem.getPosition(), 4, itemStack.getMetadata());
-                explosion.explode();
-
+                if (itemStack.getItem() == ModItems.itemAntimatterCell) {
+                    AntimatterExplosion explosion = new AntimatterExplosion(entityItem.getEntityWorld(), entityItem, entityItem.getPosition(), 4, itemStack.getMetadata());
+                    explosion.explode();
+                }
             }
         }
-
-        //}
     }
 }
