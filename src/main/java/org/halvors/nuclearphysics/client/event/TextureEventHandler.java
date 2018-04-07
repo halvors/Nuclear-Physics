4eb015d2070996c946518aa940078e31328ce938
Halvor Lyche Strandvoll <halvors@halvors.org>
2018-04-08 01:24:06 +0200

    Removed debug code.

--- src/main/java/org/halvors/nuclearphysics/client/event/TextureEventHandler.java
+++ src/main/java/org/halvors/nuclearphysics/client/event/TextureEventHandler.java
@@ -7,7 +7,6 @@ import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.fluids.Fluid;
-import org.halvors.nuclearphysics.common.NuclearPhysics;
 import org.halvors.nuclearphysics.common.Reference;
 import org.halvors.nuclearphysics.common.init.ModFluids;
 
@@ -16,7 +15,6 @@ import java.util.Map;
 
 @SideOnly(Side.CLIENT)
 public class TextureEventHandler {
-    private static final Map<FluidType, Map<Fluid, IIcon>> fluidTextureMap = new HashMap<>();
     private static final Map<String, IIcon> textureMap = new HashMap<>();
 
     public static void registerIcon(String name, TextureMap map) {
@@ -24,21 +22,20 @@ public class TextureEventHandler {
     }
 
     public static IIcon getIcon(String name) {
-        return textureMap.get(name);
+        return textureMap.get(Reference.PREFIX + name);
     }
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void preTextureHook(TextureStitchEvent.Pre event) {
         if (event.map.getTextureType() == 0) {
-            registerIcon(Reference.PREFIX + "electromagnet_edge", event.map);
-            registerIcon(Reference.PREFIX + "fulmination_generator_edge", event.map);
-            registerIcon(Reference.PREFIX + "gas_funnel_edge", event.map);
-
-            registerFluidIcon(ModFluids.deuterium, event.map);
-            registerFluidIcon(ModFluids.steam, event.map);
-            registerFluidIcon(ModFluids.tritium, event.map);
-            registerFluidIcon(ModFluids.uraniumHexaflouride, event.map);
+            registerIcon("electromagnet_edge", event.map);
+            registerIcon("fulmination_generator_edge", event.map);
+            registerIcon("gas_funnel_edge", event.map);
+            registerIcon(ModFluids.deuterium, event.map);
+            registerIcon(ModFluids.steam, event.map);
+            registerIcon(ModFluids.tritium, event.map);
+            registerIcon(ModFluids.uraniumHexaflouride, event.map);
         }
     }
 
@@ -48,71 +45,17 @@ public class TextureEventHandler {
         ModFluids.toxicWaste.setIcons(ModFluids.toxicWaste.getBlock().getIcon(0, 0));
         ModFluids.plasma.setIcons(ModFluids.plasma.getBlock().getIcon(0, 0));
 
-        setFluidIcon(ModFluids.deuterium);
-        setFluidIcon(ModFluids.uraniumHexaflouride);
-        setFluidIcon(ModFluids.steam);
-        setFluidIcon(ModFluids.tritium);
+        setIcon(ModFluids.deuterium);
+        setIcon(ModFluids.uraniumHexaflouride);
+        setIcon(ModFluids.steam);
+        setIcon(ModFluids.tritium);
     }
 
-    private void registerFluidIcon(Fluid fluid, TextureMap map) {
-        NuclearPhysics.getLogger().info("Registering texture for " + fluid.getName() + ", path: " + Reference.PREFIX + fluid.getStillIcon());
-
+    private static void registerIcon(Fluid fluid, TextureMap map) {
         registerIcon(Reference.PREFIX + "fluids/" + fluid.getName() + "_still", map);
     }
 
-    private void setFluidIcon(Fluid fluid) {
+    private static void setIcon(Fluid fluid) {
         fluid.setIcons(textureMap.get(Reference.PREFIX + "fluids/" + fluid.getName() + "_still"));
-    }
-
-    /*
-    @SubscribeEvent
-    public static void onTextureStitchEvent(TextureStitchEvent.Pre event) {
-        final TextureMap map = event.getMap();
-
-        map.registerSprite(electric_turbine_large);
-        map.registerSprite(reactorFissileMaterial);
-        textureMap.put("reactor_fissile_material", map.getTextureExtry(reactorFissileMaterial.toString()));
-    }
-
-    @SubscribeEvent
-    public static void onTextureStitchEvent(TextureStitchEvent.Post event) {
-        final TextureMap map = event.getMap();
-
-        missingIcon = map.getMissingSprite();
-        fluidTextureMap.clear();
-
-        for (FluidType type : FluidType.values()) {
-            fluidTextureMap.put(type, new HashMap<>());
-        }
-
-        for (Fluid fluid : FluidRegistry.getRegisteredFluids().values()) {
-            if (fluid.getStill() != null) {
-                fluidTextureMap.get(FluidType.STILL).put(fluid, map.getTextureExtry(fluid.getStill().toString()));
-            }
-
-            if (fluid.getFlowing() != null) {
-                fluidTextureMap.get(FluidType.FLOWING).put(fluid, map.getTextureExtry(fluid.getFlowing().toString()));
-            }
-        }
-    }
-
-    public static TextureAtlasSprite getFluidTexture(Fluid fluid, FluidType type) {
-        Map<Fluid, TextureAtlasSprite> map = fluidTextureMap.get(type);
-
-        if (fluid == null || type == null) {
-            return missingIcon;
-        }
-
-        return map.getOrDefault(fluid, missingIcon);
-    }
-
-    public static TextureAtlasSprite getTexture(String texture) {
-        return textureMap.getOrDefault(texture, missingIcon);
-    }
-    */
-
-    public enum FluidType {
-        STILL,
-        FLOWING
     }
 }
